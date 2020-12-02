2fcd66d463b52148295475d8e3e8ad32326af8c3
Guy Hershenbaum <hershi@fb.com>
2020-12-01 23:02:29 -0800

    Day 1 - parts 1+2

--- day_1/src/main.rs
+++ day_1/src/main.rs
@@ -1,3 +1,47 @@
+use std::collections::HashSet;
+use std::io::prelude::*;
+use std::io::BufReader;
+use std::fs::File;
+
+fn read_input() -> HashSet<usize> {
+    let input_file = File::open("src/input.txt").unwrap();
+    let reader = BufReader::new(input_file);
+    reader.lines()
+        .map(|x|x.unwrap().parse::<usize>().unwrap())
+        .collect::<HashSet<_>>()
+}
+
+fn find_pair(input: &HashSet<usize>, sum: usize) -> Option<(usize, usize)> {
+    let x1 = input.iter()
+        .filter(|&&x| x < sum)
+        .filter(|&x| input.contains(&(sum-x)))
+        .last();
+
+    x1.map(|x| (*x, sum-x))
+}
+
+fn part_1(input: &HashSet<usize>) {
+    let (x1, x2) = find_pair(input, 2020).unwrap();
+    println!("{} x {} == {}", x1, x2, x1 * x2);
+}
+
+fn part_2(input: &HashSet<usize>) {
+    let (x1, (x2,x3)) = input.iter()
+        .map(|x| (x, find_pair(input, 2020-x)))
+        .filter(|(_, res)| res.is_some())
+        .map(|(x, y)| (x, y.unwrap()))
+        .take(1)
+        .last()
+        .unwrap();
+
+    println!("{} x {} x {} == {}", x1, x2, x3, x1 * x2 * x3);
+}
+
 fn main() {
-    println!("Hello, world!");
+    println!("Raeding input");
+    let input = read_input();
+    println!("Input read");
+
+    part_1(&input);
+    part_2(&input);
 }
